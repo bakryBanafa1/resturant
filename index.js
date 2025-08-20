819a77cd532b0358a1b1491cde6faaa3535aab92
bakryBanafa1 <139151027+bakryBanafa1@users.noreply.github.com>
2025-08-20 13:27:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const cron = require('node-cron');
 const moment = require('moment');
 const sqlite3 = require("sqlite3").verbose();
-let db = new sqlite3.Database("/usr/src/app/newstartDB/database.db");
+let db = new sqlite3.Database("/usr/src/app/newstartDB");
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
